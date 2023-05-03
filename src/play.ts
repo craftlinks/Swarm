770f0b45b924e47a134080fe5125a005433da341
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-05-03 12:19:21 +0200

    tests

--- src/play.ts
+++ src/play.ts
@@ -1,77 +1,27 @@
-import {setTimeout} from "timers";
-
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-
-function create_array(): number[] {
-    const myArray = [];
-    const arraySize = 100;
-
-    for (let i = 0; i < arraySize; i++) {
-        myArray.push(i);
+export const myPromise: Promise<number> = new Promise((resolve, reject) => {
+    // random positive integer number
+    const randomNumber: number = Math.floor(Math.random() * 100) + 1;
+    // if even number
+    if (randomNumber % 2 === 0) {
+        resolve(randomNumber);
+        // if odd number
+    } else {
+        reject(randomNumber);
     }
-    console.log(myArray);
-    return myArray;
-}
+});
 
-let bigOak = require("./crow-tech").bigOak;
-
-function setTimeoutPromise(ms: number): Promise<number> {
-    return new Promise<number>((resolve, reject) => {
-        setTimeout(() => resolve(ms), ms);
-    });
-}
-
-function request(url: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        let done = false;
-
-        function attempt(n: number) {
-            req.open('GET', url);
-            req.send(null);
-            setTimeoutPromise(250).then(() => {
-                if (done) return;
-                else if (n < 3) attempt(n + 1);
-                else {
-                    req.abort()
-                    reject(Error("max attempts exceeded"));
-                }
-            });
-        }
-
-        const req = new XMLHttpRequest();
-
-        req.onload = () => {
-            done = true;
-            if (req.status === 200) {
-                resolve("hello");
-            } else {
-                reject(Error("Oops"));
-            }
-        };
-        req.onerror = () => {
-            done = true;
-            reject(Error('Network Error'));
-        };
-
-        attempt(1);
+export async function main(): Promise<void> {
+    const result = myPromise.then((randomNumber: number) => {
+        console.log(`Even number: ${randomNumber}`);
+        return randomNumber;
+    }).catch((randomNumber: number) => {
+        console.log(`Odd number: ${randomNumber}`);
+        return randomNumber;
     });
 
+    const number = await result;
+    console.log(`Double Number: ${number * 2}`);
 }
 
-function main() {
-    const array: number[] = create_array();
-    const ms = 500
-    setTimeout(() => console.log(ms), ms);
-    const timeout = setTimeoutPromise(ms);
-    timeout.then(value => console.log(`Got ${value}`));
-
-
-    let fifteen: Promise<number> = Promise.resolve(15);
-    fifteen.then(value => console.log(`Got ${value}`));
-
-    request('https://eloquentjavascript.net/11_async.html').then(value => console.log(`Got ${value}`)).catch(error => console.log(error));
-
-}
-
-main();
+main().then(() => {
+});
---
+++ src/tests/play.test.ts
@@ -0,0 +1,1 @@
+import {main, myPromise} from '../play';
